98edb109d1b721db9b5e44ee1804d91032ed09f3
Padman83 <endlessgalaxy15@gmail.com>
2020-05-28 02:00:52 +0800

    added helloJava

---
+++ src/README.md
@@ -0,0 +1,9 @@
+### Java_Fundamentals
+
+### 🧰 System Requirements
+    
+ * Mac , Linux or Windows.
+    
+ ### Code Editor
+    
+* [Intellij IDEA] (https://www.jetbrains.com)
---
+++ src/addTwoNumbers.java
@@ -0,0 +1,11 @@
+public class addTwoNumbers {
+    public static void main(String[] args) {
+
+        int num1 = 25;
+        int num2 = 25;
+        int sum;
+
+        sum = num1 + num2;
+        System.out.println("The sum of these numbers are: "+sum);
+    }
+}
