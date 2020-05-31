4eef9fbcb56a96f99ec9d808318459248360dc1a
Padman83 <endlessgalaxy15@gmail.com>
2020-06-01 00:55:32 +0800

    added DoubleQuotes.java

---
+++ src/DoubleQuotes.java
@@ -0,0 +1,7 @@
+public class DoubleQuotes {
+    public static void main(String[] args) {
+
+        String text = "Good Software,\"like a Wine\", it takes time.";
+        System.out.println(text);
+    }
+}
