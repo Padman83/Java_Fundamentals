56adbf129a33ce8cac20cdf51808440883b88257
Padman83 <endlessgalaxy15@gmail.com>
2020-06-04 23:47:00 +0800

    added NewLine.java

---
+++ src/NewLine.java
@@ -0,0 +1,7 @@
+public class NewLine {
+    public static void main(String[] args) {
+
+        String txt = "Simplicity\nis the ultimate sophistication. - Leonardo Da Vinci. ";
+        System.out.println(txt);
+    }
+}
