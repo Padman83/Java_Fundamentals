7ed104403b3607906916b7a6c3694c377525cd30
Padman83 <endlessgalaxy15@gmail.com>
2020-06-05 00:18:02 +0800

    added stringConcat.java

---
+++ src/stringConcat.java
@@ -0,0 +1,9 @@
+public class stringConcat {
+    public static void main(String[] args) {
+
+        String greet1 = "Minus";
+        String greet2 = "PLUS Productivity.";
+
+        System.out.println(greet1.concat(greet2));
+    }
+}
