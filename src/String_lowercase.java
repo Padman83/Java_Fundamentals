28f189a558018f33c22c986807f6a32e3c53fbc6
Padman83 <endlessgalaxy15@gmail.com>
2020-06-07 23:25:22 +0800

    added String_lowercase.java

---
+++ src/String_lowercase.java
@@ -0,0 +1,7 @@
+public class String_lowercase {
+    public static void main(String[] args) {
+
+        String txt = "CONTINUOUS IMPROVEMENT IS BETTER THAN DELAYED PERFECTION. - MARK TWAIN.";
+        System.out.println(txt.toLowerCase());
+    }
+}
