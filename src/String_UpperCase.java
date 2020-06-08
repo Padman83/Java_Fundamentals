78df34ac4254ff3db1dffa36ce77e71d292260b5
Padman83 <endlessgalaxy15@gmail.com>
2020-06-08 23:41:52 +0800

    added String_UpperCase.java

---
+++ src/String_UpperCase.java
@@ -0,0 +1,7 @@
+public class String_UpperCase {
+    public static void main(String[] args) {
+
+        String txt = "play is the highest form of research - albert einstein.";
+        System.out.println(txt.toUpperCase());
+    }
+}
