e34d6c2708c93eae5739aacbcc9282947680986e
Padman83 <endlessgalaxy15@gmail.com>
2020-06-10 01:41:33 +0800

    added Final_Variables.java

---
+++ src/Final_Variables.java
@@ -0,0 +1,11 @@
+public class Final_Variables {
+    public static void main(String[] args) {
+
+        // final keyword, used when we don't want other developer or myself to overwrite an existing value.
+        // read-only.
+        final int serialNumber = 1008;
+        System.out.println(serialNumber);
+
+    }
+
+}
