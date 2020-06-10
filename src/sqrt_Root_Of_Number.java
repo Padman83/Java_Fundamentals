ef8ded1b8946a1e96c73be5c5d8b0f85a1c066f6
Padman83 <endlessgalaxy15@gmail.com>
2020-06-11 00:16:25 +0800

    added sqrt_Root_Of_Number.java

---
+++ src/sqrt_Root_Of_Number.java
@@ -0,0 +1,6 @@
+public class sqrt_Root_Of_Number {
+    public static void main(String[] args) {
+        int number = 100;
+        System.out.println("The SquareRoot of " + number + " is " + Math.sqrt(number));
+    }
+}
