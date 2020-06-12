78e32f58bc6957e3d7be8b7986fd33ae8ff8108a
Padman83 <endlessgalaxy15@gmail.com>
2020-06-13 00:16:36 +0800

    added String_Concatenation.java

---
+++ src/String_Concatenation.java
@@ -0,0 +1,9 @@
+public class String_Concatenation {
+    public static void main(String[] args) {
+
+        String firstName = "Bart";
+        String lastName = "Simpson ;)";
+
+        System.out.println(firstName + " " + lastName);
+    }
+}
