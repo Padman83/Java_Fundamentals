360748c7ed3fc7ec170d705e8d7dce7ed61f184b
Padman83 <endlessgalaxy15@gmail.com>
2020-06-15 00:03:40 +0800

    added overwrite_Existing_Variable.java

---
+++ src/overwrite_Existing_Variable.java
@@ -0,0 +1,7 @@
+public class overwrite_Existing_Variable {
+    public static void main(String[] args) {
+        int phoneNumber = 121212;
+        phoneNumber = 7774444;
+        System.out.println(phoneNumber);
+    }
+}
