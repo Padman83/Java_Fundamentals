8131e4967cd72bbf8c49adcec0f123d653820654
Padman83 <endlessgalaxy15@gmail.com>
2020-06-14 20:07:14 +0800

    added String_Length_and_Trim.java

---
+++ src/String_Length_and_Trim.java
@@ -0,0 +1,7 @@
+public class String_Length_and_Trim {
+    public static void main(String[] args) {
+        String s1 = "The Universe";
+        System.out.println("s1 = " + s1);
+        System.out.println("The length of s1 is: "+ s1.length());
+    }
+}
