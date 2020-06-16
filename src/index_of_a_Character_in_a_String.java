bc47e8f1386f5b34430fee05758ad0dac7a7ca65
Padman83 <endlessgalaxy15@gmail.com>
2020-06-16 23:49:35 +0800

    added index_of_a_Character_in_a_String.java

---
+++ src/index_of_a_Character_in_a_String.java
@@ -0,0 +1,7 @@
+public class index_of_a_Character_in_a_String {
+    public static void main(String[] args) {
+
+        String txt = "Red Planet";
+        System.out.println(txt.indexOf(txt));
+    }
+}
