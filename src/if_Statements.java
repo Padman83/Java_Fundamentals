76aa70a7b86a3e0662330c557c1f30224fce67c7
Padman83 <endlessgalaxy15@gmail.com>
2020-06-16 23:36:00 +0800

    added if_Statements.java

---
+++ src/if_Statements.java
@@ -0,0 +1,8 @@
+public class if_Statements {
+    public static void main(String[] args) {
+        double MyAccount = 150_000_000;
+        if(MyAccount > 0) {
+            System.out.println("I am getting Richer!!!");
+        }
+    }
+}
