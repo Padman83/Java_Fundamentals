211202feaad9a743570abbba683f0431353a0f38
Padman83 <endlessgalaxy15@gmail.com>
2020-06-18 23:46:35 +0800

    Added assigning_A_Variable_Later

---
+++ src/assigning_A_Variable_Later.java
@@ -0,0 +1,7 @@
+public class assigning_A_Variable_Later {
+    public static void main(String[] args) {
+        int luckyNumber;
+        luckyNumber = 9999;
+        System.out.println(luckyNumber);
+    }
+}
