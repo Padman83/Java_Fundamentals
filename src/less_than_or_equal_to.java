c320fa73eff5d2afb2da213e52ad797461ab307d
Padman83 <endlessgalaxy15@gmail.com>
2020-06-19 00:05:56 +0800

    added less_than_or_equal_to

---
+++ src/less_than_or_equal_to.java
@@ -0,0 +1,8 @@
+public class less_than_or_equal_to {
+    public static void main(String[] args) {
+        double companyAccount = 0;
+        if(companyAccount <= 0) {
+            System.out.println("Our Company is going down in flames and we need a bailout immediately!.");
+        }
+    }
+}
