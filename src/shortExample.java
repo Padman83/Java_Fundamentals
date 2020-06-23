f2616ea2ce3e336223d38663c79ec919fa031a54
Padman83 <endlessgalaxy15@gmail.com>
2020-06-24 01:53:50 +0800

    Added shortExample.java

---
+++ src/shortExample.java
@@ -0,0 +1,7 @@
+public class shortExample {
+    public static void main(String[] args) {
+        short temperatureSouthPole = -200;
+        temperatureSouthPole = -300;
+        System.out.println(temperatureSouthPole);
+    }
+}
