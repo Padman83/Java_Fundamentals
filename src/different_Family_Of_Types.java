5665c168c58c64f0a9228989264a92723dd7a839
Padman83 <endlessgalaxy15@gmail.com>
2020-06-25 23:47:53 +0800

    added different_Family_Of_Types.java

---
+++ src/different_Family_Of_Types.java
@@ -0,0 +1,35 @@
+public class different_Family_Of_Types {
+    public static void main(String[] args) {
+
+        String passionateProgrammer = "I love what i do.";
+        int Number = 15;
+        float FloatNumber = 9.99f;
+        char character = 'P';
+        boolean isPureChocolateHealthy = true;
+        boolean isCokeHealthy = false;
+        byte myByte = 100;
+        long longNumber = 25000000000L;
+        double doubleNum = 18.88d;
+        float float1 = 35e3f;
+        double double1 = 12E4d;
+        char myEffort = 'A';
+        char a = 16, b = 13, c = 72;
+
+        System.out.println(passionateProgrammer);
+        System.out.println(Number);
+        System.out.println(FloatNumber);
+        System.out.println(character);
+        System.out.println(isPureChocolateHealthy);
+        System.out.println(isCokeHealthy);
+        System.out.println(myByte);
+        System.out.println(longNumber);
+        System.out.println(doubleNum);
+        System.out.println(float1);
+        System.out.println(double1);
+        System.out.println(myEffort);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
