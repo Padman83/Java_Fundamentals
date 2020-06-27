1b1ec10d56e40d9321fb958ca2d849909ccd9c76
Padman83 <endlessgalaxy15@gmail.com>
2020-06-27 23:57:19 +0800

    added assigning_A_Variables_Later.java

---
+++ src/assigning_A_Variables_Later.java
@@ -0,0 +1,9 @@
+public class assigning_A_Variables_Later {
+    public static void main(String[] args) {
+
+        int myNumber;
+        myNumber = 10000;
+
+        System.out.println(myNumber);
+    }
+}
