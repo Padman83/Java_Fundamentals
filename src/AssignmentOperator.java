14b6c66da1866d8d54e20c12460e23efec410d89
Padman83 <endlessgalaxy15@gmail.com>
2020-06-27 23:49:03 +0800

    added AssignmentOperator.java

---
+++ src/AssignmentOperator.java
@@ -0,0 +1,16 @@
+public class AssignmentOperator {
+    public static void main(String[] args) {
+
+        int number1;
+        int number2;
+
+        // Assigning 15 to number 1.
+        number1 = 15;
+        System.out.println(number1);
+
+        // Assigning the value of variable number2 to number1.
+        number2 = number1;
+        System.out.println(number2);
+    }
+}
+
