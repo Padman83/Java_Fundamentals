7afd81e12443c8af48010bf6cf922541ed702aee
Padman83 <endlessgalaxy15@gmail.com>
2020-06-28 00:10:15 +0800

    added block_statements.java

---
+++ src/block_statements.java
@@ -0,0 +1,8 @@
+public class block_statements {
+    public static void main(String[] args) {
+        double a;
+        double b;
+        double highestScore = 10 * 10;
+        System.out.println(highestScore);
+    }
+}
