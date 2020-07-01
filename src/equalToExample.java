f02c1ee158ac7f6d0783c9ee56d861aca4795bc3
Padman83 <endlessgalaxy15@gmail.com>
2020-07-02 00:18:43 +0800

    added equalToExample.java

---
+++ src/equalToExample.java
@@ -0,0 +1,8 @@
+public class equalToExample {
+    public static void main(String[] args) {
+        System.out.println(100 == 101);
+        System.out.println(99 == 100);
+        System.out.println(15 == 15);
+        System.out.println(108 == 108);
+    }
+}
