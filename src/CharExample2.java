9a7adc62ead83ed77712e6caae5927eed4d972db
Padman83 <endlessgalaxy15@gmail.com>
2020-07-03 23:41:13 +0800

    added CharExample2.java

---
+++ src/CharExample2.java
@@ -0,0 +1,8 @@
+public class CharExample2 {
+    public static void main(String[] args) {
+        char letter1 = '9';
+        System.out.println(letter1);
+        char letter2 = 108;
+        System.out.println(letter2);
+    }
+}
