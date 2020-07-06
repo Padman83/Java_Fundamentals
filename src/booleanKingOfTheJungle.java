6fa840c958a26c781950b11c5d4a805848a5eea0
Padman83 <endlessgalaxy15@gmail.com>
2020-07-06 23:27:41 +0800

    added booleanKingOfTheJungle.java

---
+++ src/booleanKingOfTheJungle.java
@@ -0,0 +1,8 @@
+public class booleanKingOfTheJungle {
+    public static void main(String[] args) {
+        boolean isElephantBiggerThanElephant = true;
+        boolean isLionBiggerThanElephant = false;
+        System.out.println(isElephantBiggerThanElephant);
+        System.out.println(isLionBiggerThanElephant);
+    }
+}
