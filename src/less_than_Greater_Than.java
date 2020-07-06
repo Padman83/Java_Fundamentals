0bf016391bee1c29d31598c62cf09643f05748d8
Padman83 <endlessgalaxy15@gmail.com>
2020-07-07 00:31:19 +0800

    added less_than_Greater_Than.java

---
+++ src/less_than_Greater_Than.java
@@ -0,0 +1,15 @@
+public class less_than_Greater_Than {
+    public static void main(String[] args) {
+        int rhinoWeight = 1200;
+        int rhinoTotal = 15;
+        int cleaningExpenses = 200;
+
+        if (rhinoWeight > 780) {
+            System.out.println("The Rhino is too fat for the bicycle act.");
+        }
+
+        if (rhinoTotal > 12) {
+            cleaningExpenses = cleaningExpenses + 180;
+        }
+    }
+}
