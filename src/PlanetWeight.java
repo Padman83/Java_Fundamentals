c75896185e9273d8d7177d791733d88dbac26467
Padman83 <endlessgalaxy15@gmail.com>
2020-07-09 00:22:59 +0800

    Added PlanetWeight.java

---
+++ src/PlanetWeight.java
@@ -0,0 +1,23 @@
+public class PlanetWeight {
+    public static void main(String[] args) {
+        System.out.println("My weight on Earth is ");
+        double weight = 120;
+        System.out.println(weight);
+
+        System.out.println("My weight on Mercury is ");
+        double mercury = weight  * .378;
+        System.out.println(mercury);
+
+        System.out.println("My weight on the moon is ");
+        double moon = weight * .166;
+        System.out.println(moon);
+
+        System.out.println("My weight on the Jupiter is ");
+        double jupiter = weight * 2.364;
+        System.out.println(jupiter);
+
+        System.out.println("My weight on the Neptune is ");
+        double neptune = weight * 88.9;
+        System.out.println(neptune);
+    }
+}
