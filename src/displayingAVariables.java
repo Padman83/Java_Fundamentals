418257b780b27031741c8295ae6217b23cc938b4
Padman83 <endlessgalaxy15@gmail.com>
2020-07-08 23:49:34 +0800

    added displayingAVariables.java

---
+++ src/displayingAVariables.java
@@ -0,0 +1,6 @@
+public class displayingAVariables {
+    public static void main(String[] args) {
+        String theGreatestRapper = "Tupac Shakur";
+        System.out.println(theGreatestRapper);
+    }
+}
