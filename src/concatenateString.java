a4b7ec3c5dd6b444f63aaaf63d5abb7f4d2ad686
Padman83 <endlessgalaxy15@gmail.com>
2020-07-11 00:59:36 +0800

    added concatenateString.java

---
+++ src/concatenateString.java
@@ -0,0 +1,15 @@
+public class concatenateString {
+    public static void main(String[] args) {
+        String start;
+        String middle;
+        String end;
+        String result;
+
+        start = "Java, ";
+        middle = "Kotlin";
+        end = " - is Awesome :)";
+
+        result = start + middle + end;
+        System.out.println(result);
+    }
+}
