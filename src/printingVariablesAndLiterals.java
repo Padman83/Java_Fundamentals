2b17c0fe4097fc9d62469079443f914a1040115a
Padman83 <endlessgalaxy15@gmail.com>
2020-07-10 23:42:31 +0800

    added printingVariablesAndLiterals.java

---
+++ src/printingVariablesAndLiterals.java
@@ -0,0 +1,7 @@
+public class printingVariablesAndLiterals {
+    public static void main(String[] args) {
+        double number = -10.6;
+        System.out.println(3);
+        System.out.println(number);
+    }
+}
