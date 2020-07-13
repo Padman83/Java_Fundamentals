4e1b0a4d2d58ef28e8219459bf225b64f2767839
Padman83 <endlessgalaxy15@gmail.com>
2020-07-14 00:06:41 +0800

    incrementDecrementOperator.java

---
+++ src/incrementDecrementOperator.java
@@ -0,0 +1,17 @@
+public class incrementDecrementOperator {
+    public static void main(String[] args) {
+        double number = 1.1;
+
+        System.out.println(number++);
+        System.out.println(number);
+
+        System.out.println(++number);
+        System.out.println(number);
+
+        System.out.println(number--);
+        System.out.println(number);
+
+        System.out.println(--number);
+        System.out.println(number);
+    }
+}
