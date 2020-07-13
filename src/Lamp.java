7356863519e4ee058318a3a2ee4140ee7a941d38
Padman83 <endlessgalaxy15@gmail.com>
2020-07-13 23:50:58 +0800

    added Lamp.java

---
+++ src/Lamp.java
@@ -0,0 +1,27 @@
+public class Lamp {
+
+    // instance variable.
+    private boolean isOn;
+
+    // method.
+    public void turnOn(){
+        isOn = true;
+    }
+
+    // method.
+    public void turnOff(){
+        isOn = false;
+    }
+}
+
+/*
+    Here we have created a class named Lamp.
+
+    1. The class has one variable named isOn and two methods
+       turnOn() and turnOff().
+    - These variables and methods defined within a class are called
+      members of the class.
+
+    2. We have used keywords like private and public.
+       These are known as access modifiers.
+ */
