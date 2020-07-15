d519b857af6d4e24ccb1c48a260b2c828719c82c
Padman83 <endlessgalaxy15@gmail.com>
2020-07-16 00:19:51 +0800

    added declaring_Multiple_Variables.java

---
+++ src/declaring_Multiple_Variables.java
@@ -0,0 +1,7 @@
+public class declaring_Multiple_Variables {
+    public static void main(String[] args) {
+
+        int orange = 1, apple = 0, pear = 0, strawberry = 8;
+        System.out.println(orange + apple + pear + strawberry);
+    }
+}
