f9e7cd09b3c37d32bdade109d9fda3262317cae5
Padman83 <endlessgalaxy15@gmail.com>
2020-07-15 23:53:40 +0800

    added declaringMultipleVariables.java

---
+++ src/declaringMultipleVariables.java
@@ -0,0 +1,10 @@
+public class declaringMultipleVariables {
+    public static void main(String[] args) {
+        // Declaring more than one variables of the same type, use a comma to separate it.
+        double orange = 1;
+        double apple = 2;
+        double strawberry = 8;
+        double oats = 10;
+        System.out.println(orange + apple + strawberry + oats);
+    }
+}
