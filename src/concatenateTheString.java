9c47cd8303ea9fda5b3efcffba9a60ba6fdfdeaf
Padman83 <endlessgalaxy15@gmail.com>
2020-07-17 23:50:35 +0800

    added concatenateTheString.java

---
+++ src/concatenateTheString.java
@@ -0,0 +1,15 @@
+public class concatenateTheString {
+    public static void main(String[] args) {
+        String start;
+        String middle;
+        String end;
+        String result;
+
+        start = "Talk is cheap. ";
+        middle = "Sow me the code. ";
+        end = "- Linus Torvalds. ";
+
+        result = start + middle + end;
+        System.out.println(result);
+    }
+}
